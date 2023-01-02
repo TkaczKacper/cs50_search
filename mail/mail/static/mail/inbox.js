e1d1659dd85d2d1782f1e65d22d3750b27ce0767
TkaczKacper <kacpertk2016@gmail.com>
2023-01-02 19:41:32 +0100

    train updates

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,6 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+
+
+  document.querySelector('#compose-form').addEventListener('submit', () => {
+    recipients = document.querySelector('#compose-recipients').value;
+    subject = document.querySelector('#compose-subject').value;
+    body = document.querySelector('#compose-body').value;
+    console.log(recipients, subject, body);
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      document.querySelector('#message').innerHTML = result;
+      console.log(result);
+      document.querySelector('#message').innerHTML = result.error;
+      setTimeout(1000);
+    });
+  });
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -15,7 +39,9 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#message').style.display = 'block';
 
+  document.querySelector('#message').innerHTML = '';
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -28,6 +54,17 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+  fetch(`emails/${mailbox}`).then(response => response.json())
+  .then(emails => {
+    if(emails){
+      emails.forEach(element => {
+        const content = document.createElement('div');
+        content.innerHTML = `<strong>${element.recipients}</strong>, ${element.subject}, ${element.timestamp}`;
+        document.querySelector('#emails-view').append(content); 
+      });
+    }
+    console.log(emails)
+  })
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
